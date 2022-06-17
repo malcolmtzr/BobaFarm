afd3dd5c060ec0cade16ded3ae02ad5f4176f037
Zhe Rui Tan <32306977+malcolmtzr@users.noreply.github.com>
2022-06-18 00:09:44 +0800

    Update Token Factory

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -13,36 +13,60 @@ contract TokenFactory {
     TeaToken[] public teaTokenArray;
     PearlToken[] public pearlTokenArray;
 
-    function createNewMilkToken(uint256 initialSupply) public returns (address) {
-        MilkToken milkToken = new MilkToken(initialSupply);
+    function createNewMilkToken() public returns (address) {
+        MilkToken milkToken = new MilkToken(2000000);
         milkTokenArray.push(milkToken);
         emit MilkTokenCreated(address(milkToken));
         return address(milkToken);
     }
 
-    function createNewTeaToken(uint256 initialSupply) public returns (address) {
-        TeaToken teaToken = new TeaToken(initialSupply);
+    function createNewTeaToken() public returns (address) {
+        TeaToken teaToken = new TeaToken(2500000);
         teaTokenArray.push(teaToken);
         emit TeaTokenCreated(address(teaToken));
         return address(teaToken);
     }
 
-    function createNewPearlToken(uint256 initialSupply) public returns (address) {
-        PearlToken pearlToken = new PearlToken(initialSupply);
+    function createNewPearlToken() public returns (address) {
+        PearlToken pearlToken = new PearlToken(2400000);
         pearlTokenArray.push(pearlToken);
         emit PearlTokenCreated(address(pearlToken));
         return address(pearlToken);
     }
 
-    function getMilkTokenAddressByIndex(uint256 index) public view returns (address) {
-        return address(milkTokenArray[index]);
+    function getRecentMilkTokenAddress() public view returns (address) {
+        return address(milkTokenArray[milkTokenArray.length - 1]);
     }
 
-    function getTeaTokenAddressByIndex(uint256 index) public view returns (address) {
-        return address(teaTokenArray[index]);
+    function getAllMilkTokenAddress() public view returns (address[] memory) {
+        address[] memory result = new address[](milkTokenArray.length);
+        for (uint i = 0; i <= milkTokenArray.length - 1; i++) {
+            result[i] = address(milkTokenArray[i]);
+        }
+        return result;
     }
 
-    function getPearlTokenAddressByIndex(uint256 index) public view returns (address) {
-        return address(pearlTokenArray[index]);
+    function getRecentTeaTokenAddress() public view returns (address) {
+        return address(teaTokenArray[teaTokenArray.length - 1]);
+    }
+
+    function getAllTeaTokenAddress() public view returns (address[] memory) {
+        address[] memory result = new address[](teaTokenArray.length);
+        for (uint i = 0; i <= teaTokenArray.length - 1; i++) {
+            result[i] = address(teaTokenArray[i]);
+        }
+        return result;
+    }
+
+    function getRecentPearlTokenAddress() public view returns (address) {
+        return address(pearlTokenArray[pearlTokenArray.length - 1]);
+    }
+
+    function getAllPearlTokenAddress() public view returns (address[] memory) {
+        address[] memory result = new address[](pearlTokenArray.length);
+        for (uint i = 0; i <= pearlTokenArray.length - 1; i++) {
+            result[i] = address(pearlTokenArray[i]);
+        }
+        return result;
     }
 }
