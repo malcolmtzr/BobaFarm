094adc601340b6281f8a29c83d306c3ac569f759
Zhe Rui Tan <32306977+malcolmtzr@users.noreply.github.com>
2022-06-22 19:11:55 +0800

    Updates

---
+++ precache-manifest.0f555467d387a1414778c5ecbe01732b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834116ea266375364e96139d45d97a7d",
+    "url": "/BobaFarm/index.html"
+  },
+  {
+    "revision": "ca847a103eab7235263a",
+    "url": "/BobaFarm/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e6c75cec355f53b79ac5",
+    "url": "/BobaFarm/static/js/2.fd88cf5c.chunk.js"
+  },
+  {
+    "revision": "ca847a103eab7235263a",
+    "url": "/BobaFarm/static/js/main.4a98e468.chunk.js"
+  },
+  {
+    "revision": "093889b254ea3b049947",
+    "url": "/BobaFarm/static/js/runtime-main.35df4ceb.js"
+  }
+]);
